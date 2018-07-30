53a91a56c7ec55658d1eaa360dc3ee7c81094d8c
Eyal Douwma <eyal.douwma@gmail.com>
2018-07-30 13:13:45 +0300

    done player class

---
+++ Engine/src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    private String name;
+    private char pieceShape;
+    private boolean isComputer = false;
+
+    public Player(String i_name, char i_pieceShape, boolean i_isComputer) {
+        name = i_name;
+        pieceShape = i_pieceShape;
+        isComputer = i_isComputer;
+    }
+}
+
