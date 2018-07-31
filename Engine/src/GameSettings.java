4bff972dd27e404332b850d217d486ca446e0220
Daniel <danieltl@mta.ac.il>
2018-07-31 11:34:27 +0300

    Added game settings.

---
+++ Engine/src/GameSettings.java
@@ -0,0 +1,18 @@
+public class GameSettings {
+    private enum GameType{
+        Basic,
+        MultiPlayer,
+        DynamicMultiPlayer
+    }
+    private enum Variant{
+        Regular,
+        Popout,
+        Circular
+    }
+
+    GameType gameType;
+    Variant variant;
+    int rows;
+    int columns;
+    int target;
+}
