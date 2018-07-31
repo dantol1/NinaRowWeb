9c54ea29dc8d1acc73ddb11adb5a42874a077571
Daniel <danieltl@mta.ac.il>
2018-07-31 17:46:31 +0300

    Added movement sub-class. generalised checkSequence.

--- Engine/src/Game.java
+++ Engine/src/Game.java
@@ -22,6 +22,15 @@ public class Game implements Serializable {
         Left,
         Right
     }
+    private class movement{
+        int rowMovement;
+        int colMovement;
+
+        public movement(int rowMovement, int colMovement) {
+            this.rowMovement = rowMovement;
+            this.colMovement = colMovement;
+        }
+    }
     private Board gameBoard;
     private GameSettings settings;
     private Player players[];
@@ -135,30 +144,99 @@ public class Game implements Serializable {
     private boolean checkConsecutiveDirection(DiscDirection dir, int column) {
         //TODO: check for a general approach
         boolean result = false;
-        int startingRow = (gameBoard.getNextPlaceInColumn())[column];
-        int row = startingRow;
-        int col = column;
+        int row = (gameBoard.getNextPlaceInColumn())[column];
+        int rowMovement = 0, colMovement = 0;
+        int i;
+        movement moveChange = getMovementByDirection(dir);
 
+        rowMovement = moveChange.rowMovement;
+        colMovement = moveChange.colMovement;
 
         if (settings.getVariant() == GameSettings.Variant.Regular
                 || settings.getVariant() == GameSettings.Variant.Popout) {
 
-           for (int i= 0; i<settings.getTarget(); i++) {
-
-               if (dir == DiscDirection.Down) {
-
-                 if (gameBoard.getCellSymbol(row,col) == players[activePlayerIndex].getPieceShape()) {
-                     row--;
-                 }
-                 else {
-                     break;
-                 }
-               }
+           for (i= 0; i<settings.getTarget(); i++) {
+             if (gameBoard.getCellSymbol(row,column) == players[activePlayerIndex].getPieceShape()) {
+                 row += rowMovement;
+                 column += colMovement;
+             }
+             else {
+                 break;
+             }
            }
+           if(i == settings.getTarget())
+           {
+               result = true;
+           }
+        }
+        else if(settings.getVariant() == GameSettings.Variant.Circular)
+        {
+            for (i= 0; i<settings.getTarget(); i++) {
+                if (gameBoard.getCellSymbol(row,column) == players[activePlayerIndex].getPieceShape()) {
+                    row += rowMovement;
+                    column += colMovement;
+                    row = row % settings.getRows();
+                    column = column % settings.getColumns();
+                }
+                else {
+                    break;
+                }
+            }
+            if(i == settings.getTarget())
+            {
+                result = true;
+            }
         }
 
         return result;
+    }
 
+    private movement getMovementByDirection(DiscDirection dir)
+    {
+        int rowMovement = 0, colMovement = 0;
+
+        if(dir == DiscDirection.Down)
+        {
+            rowMovement = 1;
+            colMovement = 0;
+        }
+        else if (dir == DiscDirection.Up)
+        {
+            rowMovement = -1;
+            colMovement = 0;
+        }
+        else if (dir == DiscDirection.Right)
+        {
+            rowMovement = 0;
+            colMovement = 1;
+        }
+        else if (dir == DiscDirection.Left)
+        {
+            rowMovement = 0;
+            colMovement = -1;
+        }
+        else if (dir == DiscDirection.UpperDiagonalLeft)
+        {
+            rowMovement = -1;
+            colMovement = -1;
+        }
+        else if (dir == DiscDirection.UpperDiagonalRight)
+        {
+            rowMovement = -1;
+            colMovement = 1;
+        }
+        else if (dir == DiscDirection.LowerDiagonalLeft)
+        {
+            rowMovement = 1;
+            colMovement = -1;
+        }
+        else if (dir == DiscDirection.LowerDiagonalRight)
+        {
+            rowMovement = 1;
+            colMovement = 1;
+        }
+
+        return new movement(rowMovement, colMovement);
     }
 
     private boolean checkGameTie() {
