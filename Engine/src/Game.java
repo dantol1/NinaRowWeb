e7d069231d3d6df526cf0ca6ea8c812f6e9583e1
Daniel <danieltl@mta.ac.il>
2018-08-04 21:57:33 +0300

    Removed TODO line to avoid confusion

--- Engine/src/Game.java
+++ Engine/src/Game.java
@@ -159,7 +159,6 @@ public class Game implements Serializable {
     }
 
     private boolean checkConsecutiveDirection(DiscDirection dir, int column) {
-        //TODO: Check if its within boundries of the board
         boolean result = false;
         int row = (gameBoard.getNextPlaceInColumn())[column];
         int rowMovement = 0, colMovement = 0;
